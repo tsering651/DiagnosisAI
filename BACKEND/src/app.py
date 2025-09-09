6d4c6444bd5514b2d9008658bd4dd9ea273cd637
Tsering Wangchu <tseringwangchu@Tserings-MacBook-Air.local>
2025-09-09 21:43:47 +0530

    commiteed2

--- BACKEND/src/app.py
+++ BACKEND/src/app.py
@@ -19,7 +19,11 @@ from src.utils.overlay import overlay_heatmap
 #app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-CORS(app, resources={r"/*": {"origins": "https://diagnosisai-b9xf.onrender.com/"}})
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:5173", 
+    "https://diagnosisai-b9xf.onrender.com"
+]}})
+
 
 
 
