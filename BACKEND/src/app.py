aa0e66391a47b4be873d42fce7f7db3f17f1afed
Tsering Wangchu <tseringwangchu@Tserings-MacBook-Air.local>
2025-09-09 20:35:11 +0530

    check

--- BACKEND/src/app.py
+++ BACKEND/src/app.py
@@ -77,6 +77,6 @@ def diagnose():
         traceback.print_exc()
         return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render-provided port, fallback to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5001))  # Use Render-provided port, fallback to 5000
+#     app.run(host="0.0.0.0", port=port, debug=True)
